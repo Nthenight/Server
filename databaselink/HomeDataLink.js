33a3a58dae94547c4b690ffe5a97104252d6410b
Nthenight <1102824991@qq.com>
2021-02-14 16:17:31 +0800

    首页数据和接口已经完成

---
+++ databaselink/HomeDataLink.js
@@ -0,0 +1,61 @@
+// 建立首页轮播图数据库的规则
+// 导入数据库的连接
+const mongo = require('../server/mongodblink/MongoDBLink');
+const mongoose = require('mongoose');
+const mongodblink = mongo.mongodblink;
+// 创建数据库
+const link = 'mongodb://localhost/homedata'
+mongodblink(link);
+// 创建数据库规则
+const SlideSchema = new mongoose.Schema({
+  url: String,
+  link:String
+});
+const RecommendSchema = new mongoose.Schema({
+  img: String,
+  title:{
+    type: String,
+    minlength: 2,
+    trim: true
+  },
+  link: String
+});
+const GoodsSchema = new mongoose.Schema({
+  type:String,
+  img: String,
+  link:String,
+  content: {
+    type: String,
+  },
+  price: {
+    type:Number
+  }
+});
+// 创建外键
+const ListShema = new mongoose.Schema({
+  pop: {
+    type: mongoose.SchemaTypes.ObjectId,
+    // 要关联的集合名称
+    ref:''
+  },
+  news: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref:''
+  },
+  sell: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref:''
+  }
+})
+// 使用规则
+const Slide = mongoose.model('Slide', SlideSchema);
+const Recommend = mongoose.model('Recommend', RecommendSchema);
+const Goods = mongoose.model('Goods', GoodsSchema);
+// 添加数据
+// Goods.create({img:1}).then(result=>{console.log(result)})
+
+module.exports = {
+  Slide,
+  Recommend,
+  Goods
+}
--- static/js/Connect.js
+++ static/js/Connect.js
@@ -1,7 +1,12 @@
-const homedata = require('../../jsondata/home/databaselink/HomeDataLink')
+// 导入home页面相关连接
+const homedata = require('../../databaselink/HomeDataLink')
 const homeslide = homedata.Slide;
 const homerecommend = homedata.Recommend;
+const homegoods = homedata.Goods;
+
+
 module.exports = {
   homeslide,
-  homerecommend
+  homerecommend,
+  homegoods
 }
